ba7dc8c5fb5decc089a44a00e91c9a0e2563005d
Mark Watkins <jedimark@users.sourceforge.net>
2011-06-12 21:37:41 +1000

    CodeBlocks build tweaks

--- src/version.h
+++ src/version.h
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 7;
-	static const long BUILD = 5213;
-	static const long REVISION = 11763;
+	static const long BUILD = 5214;
+	static const long REVISION = 11765;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4911;
-	#define RC_FILEVERSION 0,7,5213,11763
-	#define RC_FILEVERSION_STRING "0, 7, 5213, 11763\0"
-	static const char FULLVERSION_STRING[] = "0.7.5213.11763";
+	static const long BUILDS_COUNT = 4917;
+	#define RC_FILEVERSION 0,7,5214,11765
+	#define RC_FILEVERSION_STRING "0, 7, 5214, 11765\0"
+	static const char FULLVERSION_STRING[] = "0.7.5214.11765";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
