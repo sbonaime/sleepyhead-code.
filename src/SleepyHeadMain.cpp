8f22db5beaf4097db6fb85316667f4f44a660b6d
Mark Watkins <jedimark@users.sourceforge.net>
2011-06-14 14:43:10 +1000

    Candlesticks turned on in Linux

--- src/SleepyHeadMain.cpp
+++ src/SleepyHeadMain.cpp
@@ -947,7 +947,7 @@ void Daily::RefreshData()
         }
         wxRect r=HTMLInfo->GetRect();
         int w=r.width-27;
-  /*      ahi_bmp=G_AHI->RenderBitmap(w,25);
+        ahi_bmp=G_AHI->RenderBitmap(w,25);
         tap_bmp=TAP->RenderBitmap(w,25);
         if (ahi_bmp)
             wxMemoryFSHandler::AddFile(_T("ahi.png"), *ahi_bmp, wxBITMAP_TYPE_PNG);
@@ -959,7 +959,7 @@ void Daily::RefreshData()
             tiap_bmp=TAP_IAP->RenderBitmap(w,25);
             wxMemoryFSHandler::AddFile(_T("teap.png"), *teap_bmp, wxBITMAP_TYPE_PNG);
             wxMemoryFSHandler::AddFile(_T("tiap.png"), *tiap_bmp, wxBITMAP_TYPE_PNG);
-        } */
+        }
 
         pr=(PRTypes)cpap->summary_max(CPAP_PressureReliefType);
         if (pr==PR_NONE)
