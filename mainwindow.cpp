d808b08f2c0606b80cd233a2e27030c2ee80cee7
Mark Watkins <jedimark@users.sourceforge.net>
2011-10-02 02:02:58 +1000

    Changed check for Updates message

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -389,7 +389,7 @@ void MainWindow::replyFinished(QNetworkReply * reply)
                     QMessageBox::information(this,"Laziness Warning","I'd love to do it for you automatically, but it's not implemented yet.. :)",QMessageBox::Ok);
                 }
             } else {
-                QMessageBox::information(this,"SleepyHead v"+a,"Your already up to date!",QMessageBox::Ok);
+                QMessageBox::information(this,"SleepyHead v"+pref["VersionString"].toString(),"You're already up to date!\nLatest version on sourceforge is v"+a,QMessageBox::Ok);
             }
        }
     } else {
