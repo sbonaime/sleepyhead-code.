231e8749c6a81084f5f180e6206e8732b70952a3
Mark Watkins <jedimark@users.sourceforge.net>
2011-11-16 08:02:10 +1000

    Mac build fix take #1

--- common_gui.cpp
+++ common_gui.cpp
@@ -3,7 +3,6 @@
 
 #if QT_VERSION<0x400800
 #if defined(Q_OS_MAC)
-#include "cocoacommon.h"
 #include <Cocoa/Cocoa.h>
 #endif
 
@@ -15,9 +14,12 @@
 #include <langinfo.h>
 #endif
 
+
+// This function has been "borrowed".. Ahem..
 Qt::DayOfWeek firstDayOfWeekFromLocale()
 {
-    Qt::DayOfWeek firstDay = Qt::Monday;
+    Qt::DayOfWeek firstDay = Qt::Monday; // Fallback, acknowledging the awesome concept of Week End.
+
 #ifdef Q_OS_WIN
     WCHAR wsDay[4];
 # if defined(_WIN32_WINNT_VISTA) && WINVER >= _WIN32_WINNT_VISTA && defined(LOCALE_NAME_USER_DEFAULT)
@@ -25,8 +27,8 @@ Qt::DayOfWeek firstDayOfWeekFromLocale()
 # else
     if (GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_IFIRSTDAYOFWEEK, wsDay, 4)) {
 # endif
-    bool ok;
-    int wfd = QString::fromWCharArray(wsDay).toInt(&ok) + 1;
+        bool ok;
+        int wfd = QString::fromWCharArray(wsDay).toInt(&ok) + 1;
         if (ok) {
             firstDay = (Qt::DayOfWeek)(unsigned char)wfd;
         }
@@ -34,6 +36,7 @@ Qt::DayOfWeek firstDayOfWeekFromLocale()
 #elif defined(__GLIBC__)
     firstDay = (Qt::DayOfWeek)(unsigned char)((*nl_langinfo(_NL_TIME_FIRST_WEEKDAY) + 5) % 7 + 1);
 #elif defined(Q_OS_MAC)
+    // Unsure if this will work.. Most Mac users use 4.8 anyway, so won't see this code..
     NSCalendar *cal = [NSCalendar currentCalendar];
     int day = ([cal firstWeekday] + 5) % 7 + 1;
 
@@ -44,6 +47,7 @@ Qt::DayOfWeek firstDayOfWeekFromLocale()
 
 
 #else
+// Qt 4.8 makes this a whole lot easier
 Qt::DayOfWeek firstDayOfWeekFromLocale()
 {
     return QLocale::system().firstDayOfWeek();
--- common_gui.h
+++ common_gui.h
@@ -1,10 +1,8 @@
 #ifndef COMMON_GUI_H
 #define COMMON_GUI_H
 
-//#if QT_VERSION
 #include <QLocale>
 
 Qt::DayOfWeek firstDayOfWeekFromLocale();
-//#endif
 
 #endif
